8ff3f4ab00ed4d1fe0fe473a50b765df60b82893
CyprianSiwik <124326420+CyprianSiwik@users.noreply.github.com>
2025-02-01 18:20:10 -0500

    new code

--- ProjectPhase1.cpp
+++ ProjectPhase1.cpp
@@ -1,1 +1,11 @@
 //main cpp file
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+
+    // Additional debugging output
+    std::cout << "Current file status testing..." << std::endl;
+
+    return 0;
+}
